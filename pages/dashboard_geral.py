559a1dba7e036d6c53006084232ec85981109751
Tiago <tsbarros84@yahoo.com.br>
2026-05-11 19:22:08 -0300

    Create dashboard geral page placeholder

---
+++ pages/dashboard_geral.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+def render():
+    st.title('Dashboard Geral')
+    st.info('Módulo em construção. Esta página receberá a visão consolidada do app financeiro unificado.')
